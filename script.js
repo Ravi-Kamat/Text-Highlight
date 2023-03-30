0575768ef9e39fa8fe9a7536c8e95af4fc4e7fd6
Ravishankar Kamat <ravi061999@outlook.com>
2023-03-30 18:47:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+const paragraph = document.getElementById("text");
+const words = paragraph.innerText.split(" ");
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 8) {
+        words[i] = '<span style="background-color: yellow;">' + words[i] + '</span>';
+    }
+}
+
+paragraph.innerHTML = words.join(" ");
